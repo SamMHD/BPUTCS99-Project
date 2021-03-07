28ec0cdbe06c096a4e7a3afa1ea621fe8d6ecdad
SamMHD <mahdanian.saman@gmail.com>
2021-03-07 17:17:16 +0330

    Teacher Cources done!

--- teacher/views.py
+++ teacher/views.py
@@ -210,3 +210,186 @@ class ExercisesAnswerShow(UserPassesTestMixin, View):
         except ValueError:
             return False
 
+
+
+@method_decorator(login_required(login_url='teacher-login'), name='dispatch')
+class Exercise(UserPassesTestMixin, View):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='teacher')
+
+    def get(self, request, *args, **kwargs):
+        owner_id = get_object_or_404(Teacher, user_id=request.user.id).id
+        exercises = Exercise.objects.filter(owner_id=owner_id)
+        for i in range(len(exercises)):
+            exercises[i].due = jalali(exercises[i].due)
+            exercises[i].created_at = jalali(exercises[i].created_at)
+        return render(
+            request, 
+            'teacher/exercise/index.html',
+            {
+                'practices': exercises
+            }
+        )
+
+
+@method_decorator(login_required(login_url='teacher-login'), name='dispatch')
+class ExerciseCreate(UserPassesTestMixin, View):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='teacher')
+
+    def get(self, request, *args, **kwargs):
+        if 'date_error' in kwargs:
+            return render(request, 'teacher/exercise/create.html', {'date_error': kwargs['date_error']})
+        return render(request, 'teacher/exercise/create.html')
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        try:
+            date = list(map(int, data['date'].split("/")))
+            time = list(map(int, data['time'].split(":")))
+            g_datetime = jdatetime.datetime(
+                date[0], date[1], date[2], time[0], time[1]).togregorian()
+        except:
+            kwargs['date_error'] = 'invalid date and time'
+            return self.get(request, *args, **kwargs)
+        exercise = Exercise(
+            title=data['title'], comment=data['comment'], deadline=g_datetime)
+        exercise.save()
+        return redirect(reverse('teacher-exercises-index'))
+
+@method_decorator(login_required(login_url='teacher-login'), name='dispatch')
+class ExercisesAnswers(UserPassesTestMixin, View):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='teacher')
+
+    def get(self, request, *args, **kwargs):
+        owner_id = get_object_or_404(Teacher, user_id=request.user.id).id
+        exercise = get_object_or_404(Exercise, id=kwargs['pk'])
+        if exercise.onwer_id != owner_id:
+            raise PermissionDenied()
+        answers = Answer.objects.filter(exercise_id=kwargs['pk'])
+        for i in range(len(answers)):
+            answers[i].created_at = jalali(answers[i].created_at)
+            answers[i].student = get_object_or_404(Student, id=answers[i].student_id)
+        return render(
+            request, 
+            'teacher/exercise/answers.html', 
+            {
+                'answers': answers, 'practice_title': exercise.title
+            }
+        )
+
+
+@method_decorator(login_required(login_url='teacher-login'), name='dispatch')
+class ExercisesAnswerShow(UserPassesTestMixin, View):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='teacher')
+
+    def get_query(self, request, exercise_id, answer_id):
+        answer = get_object_or_404(Answer, id=answer_id)
+        answer.created_at = jalali(answer.created_at)
+        answer.student = get_object_or_404(Student, id=answer.student_id)
+        student = get_object_or_404(Student, id=answer.student_id)
+        
+        owner_id = get_object_or_404(Teacher, user_id=request.user.id).id
+        exercise = get_object_or_404(Exercise, id=exercise_id, owner_id=owner_id)
+        exercise.due = jalali(exercise.due)
+        return (answer, exercise, student)
+
+    def get(self, request, *args, **kwargs):
+        (answer, exercise, student) = self.get_query(request, exercise_id=kwargs['practice_id'], answer_id=kwargs['answer_id'])
+        template_data = {
+            'answer': answer,
+            'practice': exercise,
+            'student': student
+        }
+        if 'score_error' in kwargs:
+            template_data['score_error'] = kwargs['score_error']
+        return render(
+            request,
+            'teacher/exercise/answer-detail.html', 
+            template_data
+        )
+
+    def post(self, request, *args, **kwargs):
+        score = request.POST['score']
+        if not self.validate_score(score):
+            kwargs['score_error'] = 'score is invalid'
+            return self.get(request, *args, **kwargs)
+        exercise_id = kwargs['practice_id']
+
+        answer = get_object_or_404(Answer, id=kwargs['answer_id'])
+        answer.score = score
+        answer.save()
+
+        return redirect(reverse('exercises-answers', kwargs={'pk': exercise_id}))
+
+    def validate_score(self, score):
+        try:
+            val = float(score)
+            return True
+        except ValueError:
+            return False
+
+
+@method_decorator(login_required(login_url='teacher-login'), name='dispatch')
+class CoursesIndex(UserPassesTestMixin, View):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='teacher')
+
+    def get(self, request, *args, **kwargs):
+        owner_id = get_object_or_404(Teacher, user_id=request.user.id).id
+        courses = Course.objects.filter(owner_id=owner_id)
+        for i in range(len(courses)):
+            courses[i].created_at = jalali(courses[i].created_at)
+        return render(request, 'teacher/course/index.html', {'videos': courses})
+
+
+@method_decorator(login_required(login_url='teacher-login'), name='dispatch')
+class CourseCreate(UserPassesTestMixin, View):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='teacher')
+
+    def get(self, request, *args, **kwargs):
+        if 'video_error' in kwargs:
+            return render(request, 'teacher/course/create.html', {'video_error': kwargs['video_error']})
+        return render(request, 'teacher/course/create.html')
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        video = request.FILES['video']
+        print(video.size)
+        if video.content_type.find('course/') == -1:
+            kwargs['video_error'] = "wrong format"
+            return self.get(request, *args, **kwargs)
+        if video.size > settings.MAX_UPLOAD_SIZE * 1000 * 1000:
+            kwargs['video_error'] = "video size must be less than 10mg"
+            return self.get(request, *args, **kwargs)
+        course = Course(title=data['title'], video=video)
+        course.save()
+        return redirect(reverse('teacher-courses-index'))
+
+@method_decorator(login_required(login_url='teacher-login'), name='dispatch')
+class CoursesShow(UserPassesTestMixin, View):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='teacher')
+
+    def get(self, request, *args, **kwargs):
+        owner_id = get_object_or_404(Teacher, user_id=request.user.id).id
+        course = get_object_or_404(Course, id=kwargs['pk'], owner_id=owner_id)
+        return render(request, 'teacher/course/detail.html', {'video': course})
+
