e07e9c4195b4880714fa5d12d974af0b888cd1b0
SamMHD <mahdanian.saman@gmail.com>
2021-03-07 20:47:41 +0330

    endpoints bugs fixed

--- BPUTCS99Project/urls.py
+++ BPUTCS99Project/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('teachers/', include('teacher.urls')),
+    path('', include('student.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('/', include('student.urls')),
+#     path('teachers/', include('teacher.urls')),
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
